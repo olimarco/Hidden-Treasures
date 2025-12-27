a9f433fb1f022cb0b7c9b4dfb4467ce4e5031220
olimarco <olimarco999@gmail.com>
2025-12-27 17:38:41 +0100

    Added AssignCard method

--- ClasseCarta.py
+++ ClasseCarta.py
@@ -4,7 +4,7 @@ class Carta:
         self._seme = seme
         self._tipoSpeciale = tipoSpeciale
         self._coperta = True
-        self._assegnataA = Giocatore()
+        self._assegnataA = None
         self._rivelataPermanente = False
 
     def __str__(self):
@@ -12,3 +12,6 @@ class Carta:
             return f"{self._valore} di {self._seme}"
         else:
             return f"{self._tipoSpeciale}"
+        
+    def AssegnaCarta(self, giocatore):
+        self._assegnataA = giocatore
