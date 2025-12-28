49a6a2e0e64e441e48a3f4f9af2d93e404493d3c
olimarco <olimarco999@gmail.com>
2025-12-28 21:47:36 +0100

    Added information label and timer, 6x6 grid, Accept, Reject, Change, End and New Game buttons

--- tesori_nascosti_gui.py
+++ tesori_nascosti_gui.py
@@ -16,21 +16,40 @@ class DialogoNomi(EasyDialog):
 class TesoriNascosti(EasyFrame):
     def __init__(self, title = "Tesori Nascosti - Team 2", width = 1000, height = 1000, background = "#008000"):
         super().__init__(title, width, height, background)
-        self.label_risultato = self.addLabel(text="Nessun nome", row=0, column=0)
+        self.griglia_carte = []
+        self.pulsanti = []
+        self.label_info = self.addLabel(text = "In attesa dei giocatori", row = 0, column = 0, columnspan = 4, background = "#FFFF00")
+        self.label_timer = self.addLabel(text = "Tempo: 0s", row = 0, column = 4, columnspan = 2)
+        pannello_griglia = self.addPanel(row = 1, column = 0, columnspan = 6, background = "#008000")
+        for r in range(6):
+            for c in range(6):
+                indice_carta = r * 6 + c
+                pulsante = pannello_griglia.addButton(text = "?", row = r, column = c, command = lambda x = indice_carta: self.rivela_carta(x))
+                pulsante["width"] = 6
+                self.pulsanti.append(pulsante)
+        self.pulsante_accetta = self.addButton(text = "Accetta (1 PA)", row = 3, column = 0, command = self.azioneAccetta, state = "disabled") 
+        self.pulsante_rifiuta = self.addButton(text = "Rifiuta (1 PA)", row = 3, column = 1, command = self.azioneRifiuta, state = "disabled")
+        self.pulsante_cambia = self.addButton(text = "Cambia (2 PA)", row = 3, column = 2, command = self.azioneCambia, state = "disabled")
+        self.pulsante_concludi = self.addButton(text = "Concludi", row = 3, column = 3, command = self.azioneConcludi, state = "disabled")
+        self.pulsante_nuova_partita = self.addButton(text = "Nuova Partita", row = 3, column = 4, command = self.apri_dialog)
+        
+        
         self.after(100, self.apri_dialog)
 
     def apri_dialog(self):
         dialog = DialogoNomi(self)
         if dialog.modified:
             n1, n2 = dialog.risultato
-            self.label_risultato["text"] = f"Giocatori: {n1} vs {n2}"
+            self.label_info["text"] = f"Giocatori: {n1} vs {n2}"
             self.inizia_partita(n1, n2)
         else:
-            self.label_risultato["text"] = "Annullato"
+           self.quit()
+        
+    
 
+     
 
-
-
+    
 
 
 if __name__ == "__main__":
