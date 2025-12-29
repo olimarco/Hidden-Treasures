7475a6775c5510192f4b112871d03e9a34fb2ff9
olimarco <olimarco999@gmail.com>
2025-12-29 16:48:54 +0100

    Changes

--- Mazzo.py
+++ Mazzo.py
@@ -1,14 +1,15 @@
-from ClasseCarta import Carta
+from Carta import Carta
 
 class Mazzo:
     def __init__(self):
         self._mazzo = []
         self._semi = ["Picche", "Cuori", "Quadri", "Fiori"]
         self._carteSpeciali = ["Moneta", "Pergamena", "Gemma"]
-        self._valori = 13
+        self._valori = 1, 14
 
     def creaMazzo(self):
-        for i in range(self._valori):
+        self._mazzo = []
+        for i in range(*self._valori):
             for j in self._semi:
                 c = Carta(valore=i, seme=j)
                 self._mazzo.append(c)
