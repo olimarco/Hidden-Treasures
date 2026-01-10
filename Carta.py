a90c005f45676313c460c9b0cf163254443df42c
olimarco <olimarco999@gmail.com>
2026-01-10 18:16:48 +0100

    Added docstrings that explain the functioning of the class and its methods

--- Carta.py
+++ Carta.py
@@ -1,5 +1,15 @@
 class Carta:
+    """
+    Rappresenta una carta da gioco, che può essere una carta standard (valore e seme)
+    oppure una carta speciale (Moneta, Gemma o pergamena). Gestisce anche lo stato della carta
+    (coperta/scoperta) e il giocatore che la possiede.
+    """
     def __init__(self, valore=None, seme=None, tipoSpeciale=None):
+        """
+        Prende come argomenti il valore e il seme, o il tipo speciale,
+        e crea i rispettivi attributi, e crea gli attributi che serviranno a
+        gestire le altre caratteristiche della carta.
+        """
         self._valore = valore
         self._seme = seme
         self._tipoSpeciale = tipoSpeciale
@@ -8,6 +18,11 @@ class Carta:
         self._rivelataPermanente = False
 
     def __str__(self):
+        """
+        Restituisce una rappresentazione testuale della carta.
+        Gestisce inoltre i valori numerici delle figure (Jack, Regina, Re, Asso)
+        e le carte speciali.
+        """
         if self._tipoSpeciale:
             return f"{self._tipoSpeciale}"
         elif self._valore and self._seme:
@@ -18,6 +33,10 @@ class Carta:
                 return f"{self._valore} di {self._seme}"
         return "Carta non valida"
     
+    """
+    Di seguito tutti i getter e setter che servono ad accedere e modificare facilmente i 
+    valori della carta da altri file
+    """
     @property
     def valore(self):
         return self._valore
@@ -31,13 +50,23 @@ class Carta:
         return self._tipoSpeciale
 
     def assegna_carta(self, giocatore):
+        """
+        Assegna la carta a uno specifico oggetto Giocatore.
+        """
         self._assegnataA = giocatore
 
     def gira_carta(self):
+        """
+        Inverte lo stato di visibilità della carta (da coperta a scoperta e viceversa).
+        L'azione viene bloccata se la carta è stata rivelata permanentemente dalla pergamena.
+        """
         if not self._rivelataPermanente:
             self._coperta = not self._coperta
 
     def reset(self):
+        """
+        Resetta le condizioni iniziali della carta.
+        """
         self._coperta = True
         self._rivelataPermanente = False
         self._assegnataA = None
